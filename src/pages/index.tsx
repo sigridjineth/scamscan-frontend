869f418ab02c6b199540ba44bce521c2163f2d39
snoopso <mingyeongso@mingyeongs-MacBook-Air.local>
2022-08-09 23:15:59 +0900

    First commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const Feed: NextPage = () => {
         type="button"
         id="metamask"
       >
-        Connect Metamask Wallets
+        Connect Metamask Walletsssss
       </button>
     </Styled.Page>
   );
