e111142ddfdc526bff7026b41e0d394109b8c4ad
nahee-park <nahee.developer@gmail.com>
2022-08-09 22:32:01 +0900

    dev: 폰트설정

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,4 +2,8 @@ import { css } from '@emotion/react';
 import reset from 'emotion-reset';
 export const global = css`
   ${reset}
+  html, body {
+    font-size: 10px;
+    font-family: 'Syne';
+  }
 `;
