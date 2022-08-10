e6dd6ab7da00c64c7d8ffb05741d5d239ce3ce83
nahee-park <nahee.developer@gmail.com>
2022-08-11 03:44:01 +0900

    feat: 스코어바 추가

--- src/components/MintForm/InputGroup/PointSendInputGroup.tsx
+++ src/components/MintForm/InputGroup/PointSendInputGroup.tsx
@@ -1,20 +1,58 @@
 // PointSendInputGroup
 import styled from '@emotion/styled';
-import { flexColumn } from '@src/styles';
+import ScoreBar from '@src/components/common/ScoreBar';
+import { body1Regular, body2Bold, body3Regular, flexColumn } from '@src/styles';
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 
 function PointSendInputGroup() {
-  return <StRoot>PointSendInputGroup</StRoot>;
+  const {
+    register,
+    setError,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <StRoot>
+      <ScoreBar score={+41} />
+    </StRoot>
+  );
 }
 
 export default PointSendInputGroup;
 
 const StRoot = styled.section`
-  ${flexColumn}
-  justify-content: center;
+  display: flex;
+  flex-direction: column;
   width: 560px;
-  height: 660px;
+  height: 842px;
   background: #202020;
   border-radius: 20px;
   margin-top: 20px;
+  padding: 48px 48px;
+  & input {
+    background: #181818;
+    border-radius: 10px;
+    ${body1Regular};
+  }
+  & label {
+    ${body3Regular}
+    position: relative;
+    & svg {
+      position: absolute;
+      top: 45px;
+      left: 430px;
+    }
+  }
+  & button {
+    width: 100%;
+    height: 54px;
+    background: #181818;
+    border: 1px solid #2f2f2f;
+    border-radius: 10px;
+    ${body2Bold}
+    margin-top: 40px;
+  }
 `;
