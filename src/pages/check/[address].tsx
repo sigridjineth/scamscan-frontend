c7bd9da10bea3ade04357753cbaf5b823c0a1284
yj990315 <50268497+yj990315@users.noreply.github.com>
2022-08-11 02:12:30 +0900

    fix#9 check 파트 퍼블리싱 완료

--- src/pages/check/[address].tsx
+++ src/pages/check/[address].tsx
@@ -1,44 +1,19 @@
 import styled from '@emotion/styled';
+import MintingTab from '@src/components/check/MintingTab';
+import Profile from '@src/components/check/Profile';
+import ReputationTab from '@src/components/check/ReputationTab';
 import ScoreBar from '@src/components/common/ScoreBar';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const StyledRoot = styled.section`
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   background-image: url('/CheckDetailBackground.png');
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 50% 57%;
   text-align: center;
-`;
-
-const StyleProfile = styled.div`
-  width: 92px;
-  height: 92px;
-  background: url('/CheckDetailProfile.png');
-  margin-top: 100px;
-  display: inline-block;
-`;
-
-const StyledAddress = styled.div`
-  font-family: 'Syne';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 34px;
-  line-height: 120%;
-  color: #ededed;
-  text-align: center;
-  margin-top: 56px;
-  width: 630px;
-  height: 82px;
-  overflow-wrap: break-word;
-  margin-left: calc(50% - 315px);
-`;
-
-const StyledCurrentScore = styled.div`
-  margin-top: 56px;
-  color: #ededed;
 `;
 
 const StyledScore = styled.div`
@@ -52,21 +27,73 @@ const StyledScore = styled.div`
   color: #ffffff;
 `;
 
+const DashboardContainer = styled.div`
+  margin-top: 100px;
+  width: 1120px;
+  display: inline-block;
+  position: relative;
+`;
+
+interface tabTiileProps {
+  isOn: boolean;
+  left: number;
+}
+
+const TabTitles = styled.button<tabTiileProps>`
+  left: 0;
+  font-family: 'Syne';
+  font-style: normal;
+  font-weight: 800;
+  font-size: 24px;
+  line-height: 105%;
+  letter-spacing: 0.03em;
+  color: #ffffff;
+  opacity: ${({ isOn }) => {
+    if (isOn) return 1;
+    else return 0.3;
+  }};
+  left: ${({ left }) => `${left}px`};
+  position: absolute;
+`;
+
 function CheckDetail() {
   const router = useRouter();
   const [score, setScore] = useState(-90);
   const { address } = router.query;
   const [tabName, setTabName] = useState('Reputation');
+  const handleTabClick = (tabName: string) => {
+    setTabName(tabName);
+  };
 
   return (
     <StyledRoot>
-      <StyleProfile />
-      <StyledAddress>{address}</StyledAddress>
-      <StyledCurrentScore>current score</StyledCurrentScore>
+      <Profile address={address as string} />
       <StyledScore>{score}</StyledScore>
       <div className="mt-[40px]">
         <ScoreBar score={score} />
       </div>
+      <DashboardContainer>
+        <TabTitles
+          left={0}
+          isOn={tabName == 'Reputation'}
+          onClick={() => {
+            handleTabClick('Reputation');
+          }}
+        >
+          Reputation
+        </TabTitles>
+        <TabTitles
+          left={262}
+          isOn={tabName == 'Minting'}
+          onClick={() => {
+            handleTabClick('Minting');
+          }}
+        >
+          Minting
+        </TabTitles>
+        {tabName == 'Reputation' && <ReputationTab address={address as string} />}
+        {tabName == 'Minting' && <MintingTab address={address as string} />}
+      </DashboardContainer>
     </StyledRoot>
   );
 }
