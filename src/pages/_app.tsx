40bbc45528a82269fa7ec604bd9742280fd1a4a8
nahee-park <nahee.developer@gmail.com>
2022-08-11 23:59:23 +0900

    feat: 모바일뷰 우회

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@src/styles/globals.css';
 
 import { Global, ThemeProvider } from '@emotion/react';
+import styled from '@emotion/styled';
+import { body1Regular } from '@src/styles';
 import { global } from '@src/styles/global';
 import theme from '@src/styles/theme';
 import type { AppProps } from 'next/app';
@@ -8,12 +10,37 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Global styles={global} />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <StDesktop>
+        <Global styles={global} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </StDesktop>
+      <StMobile className="bg-[url('../assets/background/main.png')]">
+        Please use the desktop view 👻
+      </StMobile>
     </>
   );
 }
 
 export default MyApp;
+
+const StDesktop = styled.section`
+  width: 100%;
+  height: 100%;
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+const StMobile = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${body1Regular}
+  @media (min-width: 1025px) {
+    display: none;
+  }
+`;
